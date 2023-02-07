2e6f6376349369cc0a17f04ea60a163e42e82732
Daniel Maslov <maslov4real@gmail.com>
2023-02-07 17:07:42 +0200

    build services

---
+++ cmps/app-header.jsx
@@ -0,0 +1,12 @@
+export function AppHeader({ onSetPage }) {
+
+    return <React.Fragment>
+      <h1>Miss Books</h1>
+      <nav className="app-nav">
+        <a href="#" onClick={(ev) => onSetPage(ev, 'home')}>Home</a> |
+        <a href="#" onClick={(ev) => onSetPage(ev, 'about')}>About</a> |
+        <a href="#" onClick={(ev) => onSetPage(ev, 'book')}>Books</a>
+      </nav>
+      </ React.Fragment>
+  }
+  
